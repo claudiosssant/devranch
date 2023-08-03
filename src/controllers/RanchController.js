a2a53a766a1c49c9fef5d26a92ece198cf2d618a
Claudio Santos <claudiogsssantos@gmail.com>
2023-08-02 22:09:50 -0300

    Editando e excluindo

--- src/controllers/RanchController.js
+++ src/controllers/RanchController.js
@@ -1,6 +1,15 @@
 import House from '../models/House';
 
 class RanchController {
+
+  async index (req, res) {
+    const { status } = req.query;
+
+    const houses = await House.find ({ status });
+
+    return res.json(houses);
+  };
+
   async store (req, res) {
     const { filename } = req.file;
     const { description, price, location, status} = req.body;
@@ -17,6 +26,24 @@ class RanchController {
     });
 
     return res.json(ranches);
+  };
+
+  async update (req,res) {
+
+    const { filename } = req.file;
+    const { ranch_id } = req.params;
+    const { description, price, location, status} = req.body;
+    const { user_id } = req.headers;
+
+    const houses = await House.updateOne({ _id: ranch_id }, {
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status
+    });
+    return res.json(houses)
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,7 @@ const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/ranch', upload.single('thumbnail'), RanchController.store);
+routes.get('/ranch', RanchController.index);
+routes.put('ranch/ranch_id', upload.single('thumbnail'), RanchController.update);
 
 export default routes;
