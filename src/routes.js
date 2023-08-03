98d31477c3bd143fe4c210a9bb1696e128f95a6a
Claudio Santos <claudiogsssantos@gmail.com>
2023-08-03 19:57:56 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,6 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 routes.post('/ranch', upload.single('thumbnail'), RanchController.store);
 routes.get('/ranch', RanchController.index);
-routes.put('ranch/ranch_id', upload.single('thumbnail'), RanchController.update);
+routes.put('/ranch/:ranch_id', upload.single('thumbnail'), RanchController.update);
 
 export default routes;
